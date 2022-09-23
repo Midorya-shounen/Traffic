8e7ae06e9394f2f6d5eaff7ea74ff78776272d38
Emad Din <midorya1767@gmail.com>
2022-09-23 23:16:09 +0200

    Create errorHandle.h

---
+++ Utilities/errorHandle.h
@@ -0,0 +1,16 @@
+/*
+ * errorHandle.h
+ *
+ * Created: 9/22/2022 7:44:40 PM
+ *  Author: Emad
+ */ 
+
+
+#ifndef ERRORHANDLE_H_
+#define ERRORHANDLE_H_
+
+
+typedef enum EN_errorHanle_t{};
+
+
+#endif /* ERRORHANDLE_H_ */
