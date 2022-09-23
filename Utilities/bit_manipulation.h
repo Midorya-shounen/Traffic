9f839dd9e349e063b66881e41feb02ab8335baf9
Emad Din <midorya1767@gmail.com>
2022-09-23 23:16:07 +0200

    Create bit_manipulation.h

---
+++ Utilities/bit_manipulation.h
@@ -0,0 +1,17 @@
+/*
+ * bit_manipulation.h
+ *
+ * Created: 9/22/2022 7:44:00 PM
+ *  Author: Emad
+ */ 
+
+
+#ifndef BIT_MANIPULATION_H_
+#define BIT_MANIPULATION_H_
+
+#define set_bit(R , B) R|=(1<<B)
+#define clear_bit(R , B) R&=~(1<<B)
+
+
+
+#endif /* BIT_MANIPULATION_H_ */
